ef3ba4066e786515c9755c06df673248f2913311
Evans Ibok <evansibok@gmail.com>
2019-12-12 16:26:16 +0100

    wip

--- react-thurs/src/components/potterCard.jsx
+++ react-thurs/src/components/potterCard.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+  max-width: 400px;
+  background: red;
+`;
 
 export const PotterCard = ({ potts }) => {
-
   return (
-    <div>
+    <Container>
       <h3>Name: {potts.name}</h3>
       <p>Role: {potts.role}</p>
       <p>House: {potts.house}</p>
       <p>School: {potts.school}</p>
       <p>Blood: {potts.bloodStatus}</p>
       <p>Species: {potts.species}</p>
-    </div>
+    </Container>
   );
-}
+};
 
 export default PotterCard;
--- react-thurs/src/components/potterList.jsx
+++ react-thurs/src/components/potterList.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
 import { getPotter } from "../redux/actionCreators";
 import PotterCard from "./potterCard";
 
+const ListCon = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
 export function PotterList({ potter, getPotter }) {
-  
   useEffect(() => {
     getPotter();
-  }, []);
+  }, [getPotter]);
 
   return (
     <div>
       <h1>List of Harry Potter Characters</h1>
       <h3>Displayed using Async Redux</h3>
-      {potter &&
-        potter.map(potts => <PotterCard key={potts._id} potts={potts} />)}
+      <ListCon>
+        {potter &&
+          potter.map(potts => <PotterCard key={potts._id} potts={potts} />)
+        }
+      </ListCon>
     </div>
   );
 }
 
 function mapStateToProps(state) {
   return {
-    potter: state.potter,
+    potter: state.potter
   };
 }
 
